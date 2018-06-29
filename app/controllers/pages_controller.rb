a0b62e53a6d211a7d2a3ceca0bb10c39f4762f9f
Chloe Baron <chloe.f.baron@gmail.com>
2018-06-29 14:03:29 +0200

    App set up and landing page work started

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    pages = Page.all
+  end
+
+  def index
+    pages = Page.all
+  end
+
+  def show
+    page = Page.find(:id)
   end
 end
